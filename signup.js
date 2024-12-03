b0cfc3c1e24031705fe0cb07ddcbc85dde98e661
Vinay-ops <vbhogal5@gmail.com>
2024-12-03 15:55:38 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,23 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match!');
+        return;
+    }
+
+    if (username && email && password) {
+
+        alert('Sign up successful!');
+    } else {
+        alert('Please fill in all fields.');
+    }
+});
